1b93d04bbe23703a7c029aa2a288816f75819cda
duofresh <80326116+duofresh@users.noreply.github.com>
2024-08-30 15:03:00 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,28 +100,28 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                         The entire run recorded with source audio, any recording with altered audio will be rejected without raw footage.
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        A decent amount of attempts or gameplay before the completion.
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        The End Screen needs to be shown entirely.
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        For Geode players, use a mod menu with a cheat indicator and make sure to display it when beating a level.
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        Run stats, you either can show fps/tps and cps counters using a mod or the integrated debug setting called “show info label”, records ignoring this will be rejected.
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        Vanilla players are always required to show that they are not using geode in the same recording as the completion.
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        Clicks are REQUIRED for Insane demons and Extreme demons.
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Raw footage is REQUIRED for extreme demons and ANY verification, and you have to send your raw footage for any type of record if a list staff member asks for it.
                     </p>
                 </div>
             </div>
